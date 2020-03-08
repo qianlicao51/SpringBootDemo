0e8bb679edb1803d58d15b448ddc75c2b3d9be58
DESKTOP-C5GAJG4\Administrator <qianlicaodemo@163.com>
2020-03-08 21:59:33 +0800

    sprngdemostudy

--- demo03/src/main/java/com/example/demo03/controller/HelloController.java
+++ demo03/src/main/java/com/example/demo03/controller/HelloController.java
@@ -1,7 +1,14 @@
 package com.example.demo03.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+    @RequestMapping("/jhel")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
 }
